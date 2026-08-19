696b6d2de7e082fd817ffc85ae694de5f91544eb
Joachim Brindeau <brindeau.joachim@gmail.com>
2026-08-19 18:15:48 +0200

    fix(build): track generated Payload import map

---
+++ src/app/(payload)/admin/importMap.js
@@ -0,0 +1,54 @@
+import { default as default_b786b405fcd9412302ce8d29e6c441af } from '../../../components/ColorField'
+import { default as default_760fd892cbf0749249332edaa6bef1b4 } from '../../../components/GoogleFontField'
+import { RscEntryLexicalCell as RscEntryLexicalCell_44fe37237e0ebf4470c9990d8cb7b07e } from '@payloadcms/richtext-lexical/rsc'
+import { RscEntryLexicalField as RscEntryLexicalField_44fe37237e0ebf4470c9990d8cb7b07e } from '@payloadcms/richtext-lexical/rsc'
+import { LexicalDiffComponent as LexicalDiffComponent_44fe37237e0ebf4470c9990d8cb7b07e } from '@payloadcms/richtext-lexical/rsc'
+import { default as default_f6a0c71f8457ea7ebde630b66903be4b } from '../../../blocks/features/varMention.client'
+import { InlineToolbarFeatureClient as InlineToolbarFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
+import { OrderedListFeatureClient as OrderedListFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
+import { UnorderedListFeatureClient as UnorderedListFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
+import { LinkFeatureClient as LinkFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
+import { UnderlineFeatureClient as UnderlineFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
+import { BoldFeatureClient as BoldFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
+import { ItalicFeatureClient as ItalicFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
+import { ParagraphFeatureClient as ParagraphFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
+import { default as default_0c595d5827677db4d426c8dda5793141 } from '../../../components/RepeaterRowLabel'
+import { default as default_841f3d8d064dfe798baf7710ad67fba0 } from '../../../components/SlidePreview'
+import { default as default_134c410202d5b098d475cc919d49bc22 } from '../../../components/SlideRowLabel'
+import { default as default_0485d70ecd0925de7ff80d9fd8de19b6 } from '../../../components/TableEditor'
+import { default as default_55b275c29aa5758b3a877e29a997f50b } from '../../../components/AgentDraftButton'
+import { default as default_49d2b627e93b48e36f9059279bd151a7 } from '../../../components/BuildStatusField'
+import { default as default_374fa86aa013d2dd5771ee365c473717 } from '../../../components/ExportMenuItem'
+import { KlarcIcon as KlarcIcon_6eb899d2932d091174768945cdd618ab } from '../../../components/KlarcIcon'
+import { KlarcLogo as KlarcLogo_ffb51a65f3659371a879539c40a377fe } from '../../../components/KlarcLogo'
+import { default as default_7ab98a6ab4d9cdcce1e19618dce772d5 } from '../../../components/GoogleLoginButton'
+import { CollectionCards as CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1 } from '@payloadcms/next/rsc'
+
+/** @type import('payload').ImportMap */
+export const importMap = {
+  "/components/ColorField#default": default_b786b405fcd9412302ce8d29e6c441af,
+  "/components/GoogleFontField#default": default_760fd892cbf0749249332edaa6bef1b4,
+  "@payloadcms/richtext-lexical/rsc#RscEntryLexicalCell": RscEntryLexicalCell_44fe37237e0ebf4470c9990d8cb7b07e,
+  "@payloadcms/richtext-lexical/rsc#RscEntryLexicalField": RscEntryLexicalField_44fe37237e0ebf4470c9990d8cb7b07e,
+  "@payloadcms/richtext-lexical/rsc#LexicalDiffComponent": LexicalDiffComponent_44fe37237e0ebf4470c9990d8cb7b07e,
+  "/blocks/features/varMention.client#default": default_f6a0c71f8457ea7ebde630b66903be4b,
+  "@payloadcms/richtext-lexical/client#InlineToolbarFeatureClient": InlineToolbarFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
+  "@payloadcms/richtext-lexical/client#OrderedListFeatureClient": OrderedListFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
+  "@payloadcms/richtext-lexical/client#UnorderedListFeatureClient": UnorderedListFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
+  "@payloadcms/richtext-lexical/client#LinkFeatureClient": LinkFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
+  "@payloadcms/richtext-lexical/client#UnderlineFeatureClient": UnderlineFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
+  "@payloadcms/richtext-lexical/client#BoldFeatureClient": BoldFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
+  "@payloadcms/richtext-lexical/client#ItalicFeatureClient": ItalicFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
+  "@payloadcms/richtext-lexical/client#ParagraphFeatureClient": ParagraphFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
+  "/components/RepeaterRowLabel#default": default_0c595d5827677db4d426c8dda5793141,
+  "/components/SlidePreview#default": default_841f3d8d064dfe798baf7710ad67fba0,
+  "/components/SlideRowLabel#default": default_134c410202d5b098d475cc919d49bc22,
+  "/components/TableEditor#default": default_0485d70ecd0925de7ff80d9fd8de19b6,
+  "/components/AgentDraftButton#default": default_55b275c29aa5758b3a877e29a997f50b,
+  "/components/BuildStatusField#default": default_49d2b627e93b48e36f9059279bd151a7,
+  "/components/ExportMenuItem#default": default_374fa86aa013d2dd5771ee365c473717,
+  "/components/KlarcIcon#KlarcIcon": KlarcIcon_6eb899d2932d091174768945cdd618ab,
+  "/components/KlarcLogo#KlarcLogo": KlarcLogo_ffb51a65f3659371a879539c40a377fe,
+  "/components/GoogleLoginButton#default": default_7ab98a6ab4d9cdcce1e19618dce772d5,
+  "@payloadcms/next/rsc#CollectionCards": CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1
+}
